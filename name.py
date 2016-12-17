86b32b1a317cbedb7783c1036624bdda4afb0fee
codrcodz <cody.l.cochran@gmail.com>
2016-12-17 10:07:45 +0300

    updated name.py

--- name.py
+++ name.py
@@ -1,4 +1,7 @@
-name = "ada lovelace"
+first_name = "ada"
+last_name = "lovelace"
+name = first_name + " " + last_name
 print("Title: " + name.title())
 print("Upper: " + name.upper())
 print("Lower: " + name.lower())
+print("Hello, " + name.title() + "!")
